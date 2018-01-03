fa9c1b562f3327f0ca6bbbb25c5bacdc9c49406b
Silvia Chung <schung97@gmail.com>
2018-01-03 15:33:19 -0500

    silvia changes

--- frontend/src/events.js
+++ frontend/src/events.js
@@ -2,7 +2,31 @@ class Event{
 
   static handlers() {
     document.getElementById('login-form').addEventListener('submit', Event.login);
-    document.getElementById("form-div").addEventListener('submit', Event.addEventListener);
+    document.getElementById('form-div').addEventListener('submit', Event.addFormListener);
+    document.getElementsByClassName('navbar')[0].addEventListener('click', Event.display);
+  }
+
+  static display(event) {
+    switch(event.target.innerText) {
+      case 'Edit Category':
+          document.getElementById('edit-proposed-budget-div').style.display = 'initial';
+          document.getElementById('create-proposed-budget-div').style.display = 'none';
+          document.getElementById('add-transaction-div').style.display = 'none';
+          document.getElementById('pie').style.display = 'none';
+          break;
+      case 'Create Budget':
+          document.getElementById('create-proposed-budget-div').style.display = 'initial';
+          document.getElementById('edit-proposed-budget-div').style.display = 'none';
+          document.getElementById('add-transaction-div').style.display = 'none';
+          document.getElementById('pie').style.display = 'none';
+          break;
+      case 'Add Purchase':
+          document.getElementById('add-transaction-div').style.display = 'initial';
+          document.getElementById('edit-proposed-budget-div').style.display = 'none';
+          document.getElementById('create-proposed-budget-div').style.display = 'none';
+          document.getElementById('pie').style.display = 'none';
+          break;
+    }
   }
 
   static addFormListener(event){
@@ -19,14 +43,12 @@ class Event{
 
     User.findOrCreateUser(firstname, lastname, email).then(function(user) {
       document.getElementById('user').innerHTML = `Hi, ${firstname} ${lastname}!`
-      document.getElementById('login-form').style.display = 'none';
+      document.getElementsByClassName('login')[0].style.display = 'none';
+      document.getElementsByClassName('jumbotron')[0].style.display = 'initial';
+      document.getElementById('edit-proposed-budget-div').style.display = 'none';
+      document.getElementById('create-proposed-budget-div').style.display = 'none';
+      document.getElementById('add-transaction-div').style.display = 'none';
     })
   }
-
-  static handlers() {
-    console.log(document.getElementsByClassName('login')[0])
-    document.getElementsByClassName('login')[0].addEventListener('submit', Event.login);
-  }
-
 
 }
