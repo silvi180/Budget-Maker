dd21d3f1d046ccf25966785b925dc305ce4d3759
Silvia Chung <schung97@gmail.com>
2018-01-03 10:45:02 -0500

    login

--- frontend/src/adapter.js
+++ frontend/src/adapter.js
@@ -6,7 +6,18 @@ class Adapter {
   }
 
   static getUsers() {
-    return fetch('http://localhost:3000/api/users').then(response => response.json());
+    return fetch('http://localhost:3000/api/users').then(response => response.map( item => item.json()));
+  }
+
+  static createUser(fields) {
+    return fetch(`http://localhost:3000/api/users`, {
+      method: 'POST',
+      body: JSON.stringify(fields),
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }).then(response => response.json()).then(json => new User(json));;
   }
 
   static updateUser(id, fields) {
@@ -24,5 +35,57 @@ class Adapter {
     return fetch(`http://localhost:3000/api/users/${id}`, { method: 'DELETE' }).then(response => response.json());
   }
 
-  
+  static createPurchase(fields) {
+    return fetch(`http://localhost:3000/api/purchases`, {
+      method: 'POST',
+      body: JSON.stringify(fields),
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }).then(response => response.json()).then(json => new Purchase(json));;
+  }
+
+  static updatePurchase(id, fields) {
+    return fetch(`http://localhost:3000/api/purchases/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(fields),
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }).then(response => response.json()).then(json => new Purcahse(json));;
+  }
+
+  static deletePurchase(id) {
+    return fetch(`http://localhost:3000/api/purchases/${id}`, { method: 'DELETE' }).then(response => response.json());
+  }
+
+  static createProposed(fields) {
+    return fetch(`http://localhost:3000/api/proposed`, {
+      method: 'POST',
+      body: JSON.stringify(fields),
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }).then(response => response.json()).then(json => new Proposed(json));;
+  }
+
+  static updateProposed(id, fields) {
+    return fetch(`http://localhost:3000/api/proposed/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(fields),
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      }
+    }).then(response => response.json()).then(json => new Proposed(json));;
+  }
+
+  static deletePurchase(id) {
+    return fetch(`http://localhost:3000/api/purchases/${id}`, { method: 'DELETE' }).then(response => response.json());
+  }
+
+
 }
--- frontend/src/event.js
+++ frontend/src/event.js
@@ -9,7 +9,11 @@ class Event {
     const firstname = event.target.firstname.value ;
     const lastname = event.target.lastname.value;
     const email = event.target.email.value;
+    User.findOrCreateUser(firstname, lastname, email).then(function(user) {
 
-    document.getElementsByClassName('login')[0].style.display = 'none';
+      
+      document.getElementsByClassName('login')[0].style.display = 'none';
+    });
+
   }
 }
